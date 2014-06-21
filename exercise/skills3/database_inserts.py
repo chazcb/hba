03a495b79049994f79dfcb1d33da0fbdd364e8ec
Vivien Chan <vivienwchan@hotmail.com>
2014-06-20 17:04:41 -0700

    implemented csv.reader module

--- exercise/skills3/database_inserts.py
+++ exercise/skills3/database_inserts.py
@@ -1,4 +1,5 @@
 import sqlite3
+import csv
 
 DB = None
 CONN = None
@@ -13,16 +14,32 @@ def initialize_customers_table():
 	pass
 
 def customers_table_append(row):
-	pass
-
-def read_customers_csv(filename):
-	f = open(filename)
-	for row in f:
-		row.rstrip()
-		fields = row.split(",")
-		print fields
+	# customer_id 
+ #    first 
+ #    last 
+ #    email 
+ #    telephone 
+ #    called 
+	# query = """INSERT into customers values(?,?,?,?,?,?)"""
+ #    DB.execute(query, (first_name, last_name, github)) #create new row
+ #    CONN.commit()
+    pass
 	
+def read_customers_csv(filename):
 
+	f = open(filename)
+
+	reader = csv.reader(f)  # call reader function to parse and remove \n
+	header_list = reader.next()  # read in first line to get header
+
+	customer = {}
+
+	print header_list
+	for row in reader:
+		row = [x.strip() for x in row if x]
+
+
+		customers_table_append(row)
 
 def main():
     connect_to_db()
