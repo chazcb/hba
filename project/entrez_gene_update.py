0b173301804fa4970047bb281f43abacb86a5d70
Vivien Chan <vivienwchan@hotmail.com>
2014-07-08 00:24:22 -0700

    successfully append to genes table

--- project/entrez_gene_update.py
+++ project/entrez_gene_update.py
@@ -1,62 +1,77 @@
 import gzip
+import os
+import model
 from urllib import urlretrieve
-from urllib2 import urlopen
-from datetime import date
+from datetime import date, datetime
+from sqlalchemy import func
 
-def read_human_gene_info_urllib():
-# This writes out a file into the local file directory
+def read_human_gene_info():
 
-    timestamp = date.today().isoformat()
-
-    # filenames
     gene_info_filename = "Homo_sapiens_gene_info.gz" 
-    output_filename = "gene_id_sym_desc.txt"
 
-    # retrieve file from ncbi ftp site
+    # retrieve file from ncbi ftp site, then write out a file into the local file directory
     urlretrieve("ftp://ftp.ncbi.nlm.nih.gov/gene/DATA/GENE_INFO/Mammalia/Homo_sapiens.gene_info.gz",
         gene_info_filename)
 
-    # file handles
     gene_info = gzip.open(gene_info_filename)
-    output_file = open(output_filename, 'w')
-    # creates file if does not exist, overwrite existing
+
+    os.remove(gene_info_filename)
+
+    return(gene_info)
+
+def append_gene_table(db_session, gene_info):
+
+    timestamp = date.today().isoformat()
+    output_filename = timestamp + "_gene_id_sym_desc.txt"
+    output_file = open(output_filename, 'w')  # creates file if does not exist, overwrite existing
 
     # read in first line to get header
     header = gene_info.readline()
-    header.rstrip()
-    header_list = header.split()
+    header_list = header.rstrip().split()
     # field names in header starts after first element ("Format:")
     geneid_index = header_list.index("GeneID")-1
     symbol_index = header_list.index("Symbol")-1
     synonym_index = header_list.index("Synonyms")-1
     desc_index = header_list.index("description")-1
 
-    column_name = "RowID, GeneID, Symbol, Synonyms, Description, Version\n"
+    column_name = "RowID\tGeneID\tSymbol\tSynonyms\tDescription\tVersion\n"
     output_file.write(column_name)
 
-    # write each row into output file
-    i = 1
+    # write each row into db and output file
+
+    if db_session.query(func.max(model.Gene.id)).one()[0]:
+        i = db_session.query(func.max(model.Gene.id)).one()[0]
+    else:
+        i = 0
+
     for line in gene_info:
-        item = line.rstrip().split()
-        datarow = ("%s, %s, %s, %s, %s, %s\n") % (i, item[geneid_index], item[symbol_index], item[synonym_index], item[desc_index], timestamp)
-        output_file.write(datarow)
         i += 1
+        item = line.rstrip().split()
+        # write to db
+        gene = model.Gene(id = i, 
+                    entrez_gene_id = item[geneid_index],
+                    entrez_gene_symbol = item[symbol_index],
+                    entrez_gene_synonym = item[synonym_index],
+                    entrez_gene_desc = item[desc_index], 
+                    entrez_version = datetime.utcnow()
+                    )
+        db_session.add(gene)
+        # write to file
+        datarow = ("%s\t%s\t%s\t%s\t%s\t%s\n") % (i, 
+            item[geneid_index], item[symbol_index], item[synonym_index], item[desc_index], timestamp)
+        output_file.write(datarow)
+
+    db_session.commit()
 
     output_file.close()
     gene_info.close()
 
     print "Successfully created %s" % output_filename
 
-def read_human_gene_info_urllib2():
-# urlopen avoids writing out a file in the local file directory
-# gzip not working in this function
-    info_file = urlopen("ftp://ftp.ncbi.nlm.nih.gov/gene/DATA/GENE_INFO/Mammalia/Homo_sapiens.gene_info.gz")
-
-    output_file = gzip.info_file
-
 def main():
 
-    read_human_gene_info_urllib()
+    gene_info = read_human_gene_info()
+    append_gene_table(model.db_session, gene_info)
 
 if __name__ == "__main__":
     main()
