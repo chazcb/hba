610ae8b38bc4278d692f54b302057a47a7270744
Vivien Chan <vivienwchan@hotmail.com>
2014-07-07 20:55:21 -0700

    renamed tables to follow naming convention

--- project/model.py
+++ project/model.py
@@ -1,9 +1,9 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey
 from sqlalchemy.orm import sessionmaker, relationship, backref, scoped_session
 
-ENGINE = create_engine("sqlite:///repo.db", echo=False)
+ENGINE = create_engine("sqlite:///repo.db", echo=True)
 db_session = scoped_session(sessionmaker(bind=ENGINE,
                                        autocommit = False,
                                        autoflush = False))
@@ -11,16 +11,17 @@ db_session = scoped_session(sessionmaker(bind=ENGINE,
 Base = declarative_base()
 Base.query = db_session.query_property()
 
-### Class declarations go here
+### Class declarations 
+# entity classes
 class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key = True)
-    email = Column(String(50), nullable = True)
-    username = Column(String(50), nullable = True)
-    password = Column(String(50), nullable = True)
+    username = Column(String(50), nullable = False)
+    password = Column(String(50), nullable = False)
     firstname = Column(String(50), nullable = True)
     lastname = Column(String(50), nullable = True)
+    email = Column(String(50), nullable = True)
 
 class Gene(Base):
     __tablename__ = "genes"
@@ -28,6 +29,7 @@ class Gene(Base):
     id = Column(Integer, primary_key = True)
     entrez_gene_id = Column(Integer, nullable = False)
     entrez_gene_symbol = Column(String(50), nullable = True)
+    entrez_gene_synonym = Column(String(255), nullable = True)
     entrez_gene_desc = Column(String(255), nullable = True)
     entrez_version = Column(DateTime, nullable = False)
 
@@ -35,20 +37,20 @@ class List(Base):
     __tablename__ = "lists"
 
     id = Column(Integer, primary_key = True)
-    user_id = Column(Integer, ForeignKey('users.id'))
+    user_id = Column(Integer, ForeignKey('users.id'), nullable = False)
     title = Column(String(255), nullable = True)
     description = Column(String(255), nullable = True)
     date_created = Column(DateTime, nullable = False)
-    file_loc = Column(String(255), nullable = False)
+    file_obj = Column(Text, nullable = True)
 
     user = relationship("User",
-            backref=backref("collections", order_by=id))
+            backref=backref("lists", order_by=id))
 
 class Collection(Base):
     __tablename__ = "collections"
 
     id = Column(Integer, primary_key = True)
-    user_id = Column(Integer, ForeignKey('users.id'))
+    user_id = Column(Integer, ForeignKey('users.id'), nullable = False)
     description = Column(String(255), nullable = True)
     date_created = Column(DateTime, nullable = False)
 
@@ -63,92 +65,84 @@ class Tag(Base):
 
 # cross tables
 
-class Tag2user(Base):
-    __tablename__ = "tag_x_user"
+class User_tag(Base):
+    __tablename__ = "user_tag"
 
     id = Column(Integer, primary_key = True)
-    tag_id = Column(Integer, ForeignKey('tags.id'))
-    user_id = Column(Integer, ForeignKey('users.id'))
+    tag_id = Column(Integer, ForeignKey('tags.id'), nullable = False)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable = False)
 
     tag = relationship("Tag",
-            backref=backref("tag_x_user", order_by=id))
+            backref=backref("user_tag", order_by=id))
     user = relationship("User",
-            backref=backref("tag_x_user", order_by=id))
+            backref=backref("user_tag", order_by=id))
 
-class Tag2list(Base):
-    __tablename__ = "tag_x_list"
+class List_tag(Base):
+    __tablename__ = "list_tag"
 
     id = Column(Integer, primary_key = True)
-    tag_id = Column(Integer, ForeignKey('tags.id'))
-    list_id = Column(Integer, ForeignKey('lists.id'))
+    tag_id = Column(Integer, ForeignKey('tags.id'), nullable = False)
+    list_id = Column(Integer, ForeignKey('lists.id'), nullable = False)
 
     tag = relationship("Tag",
-            backref=backref("tag_x_list", order_by=id))
+            backref=backref("list_tag", order_by=id))
     lists = relationship("List",
-            backref=backref("tag_x_list", order_by=id))
+            backref=backref("list_tag", order_by=id))
 
-class List2access(Base):
-    __tablename__ = "list_x_access"
+class List_access(Base):
+    __tablename__ = "list_access"
 
     id = Column(Integer, primary_key = True)
-    list_id = Column(Integer, ForeignKey('lists.id'))
-    user_id = Column(Integer, ForeignKey('users.id'))
+    list_id = Column(Integer, ForeignKey('lists.id'), nullable = False)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable = False)
 
     lists = relationship("List",
-            backref=backref("list_x_access", order_by=id))
+            backref=backref("list_access", order_by=id))
     user = relationship("User",
-            backref=backref("list_x_access", order_by=id))
+            backref=backref("list_access", order_by=id))
 
-class User2list(Base):
-    __tablename__ = "user_x_list"
+class List_user(Base):
+    __tablename__ = "list_user"
 
     id = Column(Integer, primary_key = True)
-    user_id = Column(Integer, ForeignKey('users.id'))
-    list_id = Column(Integer, ForeignKey('lists.id'))
+    user_id = Column(Integer, ForeignKey('users.id'), nullable = False)
+    list_id = Column(Integer, ForeignKey('lists.id'), nullable = False)
 
     user = relationship("User",
-            backref=backref("user_x_list", order_by=id))
+            backref=backref("list_user", order_by=id))
     lists = relationship("List",
-            backref=backref("user_x_list", order_by=id))
+            backref=backref("list_user", order_by=id))
 
-class List2gene(Base):
-    __tablename__ = "list_x_gene"
+class List_gene(Base):
+    __tablename__ = "list_gene"
 
     id = Column(Integer, primary_key = True)
-    list_id = Column(Integer, ForeignKey('lists.id'))
-    gene_id = Column(Integer, ForeignKey('genes.id'))
+    list_id = Column(Integer, ForeignKey('lists.id'), nullable = False)
+    gene_id = Column(Integer, ForeignKey('genes.id'), nullable = False)
 
     lists = relationship("List",
-            backref=backref("list_x_gene", order_by=id))
+            backref=backref("list_gene", order_by=id))
     gene = relationship("Gene",
-            backref=backref("list_x_gene", order_by=id))
+            backref=backref("list_gene", order_by=id))
 
-class Collection2list(Base):
-    __tablename__ = "collection_x_list"
+class List_collection(Base):
+    __tablename__ = "list_collection"
 
     id = Column(Integer, primary_key = True)
-    collection_id = Column(Integer, ForeignKey('collections.id'))
-    list_id = Column(Integer, ForeignKey('lists.id'))
+    collection_id = Column(Integer, ForeignKey('collections.id'), nullable = False)
+    list_id = Column(Integer, ForeignKey('lists.id'), nullable = False)
 
     collection = relationship("Collection",
-            backref=backref("collection_x_list", order_by=id))
+            backref=backref("list_collection", order_by=id))
     lists = relationship("List",
-            backref=backref("collection_x_list", order_by=id))
+            backref=backref("list_collection", order_by=id))
 
 ### End class declarations
 
-# def connect():
-#     global ENGINE
-#     global Session
-
-#     ENGINE = create_engine("sqlite:///repo.db", echo=False)
-#     Session = sessionmaker(bind=ENGINE)
-
-#     return Session()
-
-
 def main():
-    pass
+
+    # initialize database
+    Base.metadata.create_all(ENGINE)
 
 if __name__ == "__main__":
     main()
